395e2cde3da63359b26f89d57a240d21a715f369
Xingyao Wang <xingyao6@illinois.edu>
2023-08-03 17:26:21 -0500

    update script

--- scripts/run_llama2_worker_multigpu.sh
+++ scripts/run_llama2_worker_multigpu.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# CUDA_VISIBLE_DEVICES=0,2,3 ./scripts/run_llama2_worker_multigpu.sh meta-llama/Llama-2-13b-chat-hf 3 --limit-worker-concurrency 10
+
 SERVER_IDX=2
 SERVER_PORT=$((31000 + $SERVER_IDX))
 MODEL=$1  # e.g., meta-llama/Llama-2-13b-chat-hf
+N_GPUS=$2
+EXTRA_ARGS="${@:3}"
 echo $MODEL
 export FASTCHAT_WORKER_API_TIMEOUT=360
 python3 -m fastchat.serve.model_worker \
@@ -11,9 +15,10 @@ python3 -m fastchat.serve.model_worker \
     --port $SERVER_PORT \
     --worker http://0.0.0.0:$SERVER_PORT \
     --controller http://localhost:31001 \
-    --limit-worker-concurrency 10 \
     --max-gpu-memory 15GB \
-    --num-gpus 2 \
+    --num-gpus $N_GPUS \
+    $EXTRA_ARGS
+
     # --load-8bit \
     # --cpu-offloading
     # --load-4bit \
